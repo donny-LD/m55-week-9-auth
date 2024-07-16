dc5ad5fab60a01aaf3318017a0239d99aaee638a
mukhtar mohamed <170721974+donny-LD@users.noreply.github.com>
2024-07-16 12:19:08 +0100

    feature: basic user riute done

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -1,47 +1,61 @@
 // const messages = require("dote/src/messages")
 
-const isData = async ( req, res , next) => {
-    console.log("isData middleware hit and usrname: ", req.body.username);
-    try {
-
-        if (!req.body.username) {
-            res.status(422).json ({ message: "data is incomplete"});
-            return;
-        }
-        next ();
-
-    } catch (error) {
-        res.status(500).json ({message: error.message, error:error})
-    }
-
-};
-// check if string is lowercase and makes cover 
-const isLowerCase = async ( req, res, next) => {
-    try{
-//code here
-    }catch (error) {
-        res.status(500).json ({message: error.message, error:error})
-}
-};
-// check if email is valid using regex
-const isValidEmail = async (req, res, next) => {
-
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const isData = async (req, res, next) => {
+  console.log("isData middleware hit and usrname: ", req.body.username);
   try {
-
-    //code here 
-       if (!emailRegex.test(email)) {
-         return res.status(400).json({ message: "Invalid email format" });
-       }
-
-
-       next();
+    if (!req.body.username) {
+      res.status(422).json({ message: "data is incomplete" });
+      return;
+    }
+    next();
   } catch (error) {
     res.status(500).json({ message: error.message, error: error });
   }
 };
+// check if string is lowercase and makes cover
+const isLowerCase = async (req, res, next) => {
+  try {
+    //code here
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+// check if email is valid using regex
+const isValidEmail = async (req, res, next) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  try {
+    //code here
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    next();
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
+const isPassword = async (req, res, next) => {
+  try {
+    const { password } = req.body;
+
+    if (!password) {
+      return res.status(400).json({ message: "Password is required" });
+    }
+
+    if (!password.test(password)) {
+      return res.status(400).json({ message: "Invalid password format" });
+    }
+
+    next();
+  } catch (error) {
+    res.status(500).json({ message: error.message, error });
+  }
+};
+
 module.exports = {
-    isData,
-    isLowerCase,
-    isValidEmail
-}
+  isData,
+  isLowerCase,
+  isValidEmail,
+  isPassword,
+};
---
+++ src/users/routes.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const userRouter = Router();
+
+const { register, login, getAllUsers} = require ("./controllers");
+
+
+userRouter.post("/register", register);
+userRouter.post("/login", login);
+
+userRouter.get("/getAllUser", getAllUsers);
+
+module.exports = userRouter
